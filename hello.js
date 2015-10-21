1bad7de140eb0ae53468563873159835c54d4e55
pepeotad <pepeotad@users.noreply.github.com>
2015-10-20 22:19:35 -0400

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,12 @@
+var hi = new Array ();
+hi[0] = "The cutest cocks on the internet!";
+hi[1] = "Take the gun, leave the cannoli!";
+hi[2] = "The hets are out of control!";
+hi[3] = "[SCREAMING]";
+hi[4] = "Jet fuel can't melt steel beams!";
+hi[5] = "Now available in 480p!";
+hi[6] = "This is our swamp!";
+hi[7] = "Talk shit get hit!";
+var i = Math.floor(8*Math.random())
+//this number ^ must always be one more than however many greetings there are!!!
+document.write(hi[i]);
